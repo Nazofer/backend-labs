71aeebac92e77e56c1609257696d479d956e6bec
Nazik <nvovk.2004@gmail.com>
2023-11-26 16:13:03 +0200

    fix: remove unused code

--- src/utils/getStatus.js
+++
@@ -1,7 +0,0 @@
-const statuses = ['working', 'done', 'pending', 'failed', 'error'];
-
-export const getStatus = () => {
-  const status = statuses[Math.floor(Math.random() * statuses.length)];
-
-  return status;
-}
